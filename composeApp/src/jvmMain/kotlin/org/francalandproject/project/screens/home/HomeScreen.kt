55386ae651d2dc563db8a825b0198b687a959abb
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-12-17 01:57:33 -0300

    new_era_commited

--- composeApp/src/jvmMain/kotlin/org/francalandproject/project/screens/home/HomeScreen.kt
+++ composeApp/src/jvmMain/kotlin/org/francalandproject/project/screens/home/HomeScreen.kt
@@ -10,9 +10,9 @@ import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import org.francalandproject.project.components.TopBarWithTabs
 import org.francalandproject.project.repository.ClientRepository
-import org.francalandproject.project.screens.debitsLayer.ConsultDebitsScreen
-import org.francalandproject.project.screens.contractsLayer.ContractsScreen
 import org.francalandproject.project.screens.buyerLayer.BuyersScreen
+import org.francalandproject.project.screens.contractsLayer.ContractsScreen
+import org.francalandproject.project.screens.debitsLayer.ConsultDebitsScreen
 
 
 @Composable
